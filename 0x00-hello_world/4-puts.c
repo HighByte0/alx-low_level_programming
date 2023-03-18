e1cb05db34d08cb752f2bb7489b0ce059c74568d
byhardi <you@example.com>
2023-03-17 18:06:36 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * programme that print "Programming is like building a multilingual puzzl
+ * return wirth 0
+ * */
 int main(void)
 {
 puts("/"Programming is like building a multilingual puzzle");
